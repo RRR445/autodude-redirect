add89fca248ffce16c8e6c19a3fccda4820ac479
RRR445 <rauli.aho@gmail.com>
2026-04-14 14:47:39 +0300

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,8 +1,8 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const slot = parseInt(req.query.slot);
 
   const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTXl_5Pil_OW6Du4iL5BG7Cnp4_d1eC-2jbfxgS2pax9piqalt0RwAgtyZI6P11bobUQUkkYaOg9RNh/pub?gid=2120963522&single=true&output=csv';
-  
+
   const response = await fetch(csvUrl);
   const text = await response.text();
 
