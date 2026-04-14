0705648a444d3261f4bc107b91c7402227b85f74
RRR445 <rauli.aho@gmail.com>
2026-04-14 14:41:31 +0300

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,16 +1,14 @@
 export default async function handler(req, res) {
   const slot = parseInt(req.query.slot);
 
-  const SHEET_ID = '1hjoRWF5HtV-mXlIHdbzyYRUJWQqu_p91zNFohSjxtEI';
-  const GID = '2120963522';
-
-  const csvUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv&gid=${GID}`;
+  const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTXl_5Pil_OW6Du4iL5BG7Cnp4_d1eC-2jbfxgS2pax9piqalt0RwAgtyZI6P11bobUQUkkYaOg9RNh/pub?gid=2120963522&single=true&output=csv';
+  
   const response = await fetch(csvUrl);
   const text = await response.text();
 
   const rows = text.trim().split('\n');
-  const row = rows[slot].split(',');
-  const url = row[3].replace(/"/g, '').trim();
+  const cols = rows[slot].split(',');
+  const url = cols[3].replace(/"/g, '').trim();
 
   res.redirect(302, url);
 }
